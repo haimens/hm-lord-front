2ade55503dae8226ed98852006ff46a834331b38
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-20 12:56:09 -0700

    Update Driver.container.js

--- src/containers/driver/Driver.container.js
+++ src/containers/driver/Driver.container.js
@@ -3,6 +3,18 @@ import React, { Component } from "react";
 class Driver extends Component {
   render() {
     return <div>Driver</div>;
+                <div>Driver #1000</div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="p-3 bg-white">
+                <div>Driver #1000</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
 export default Driver;
