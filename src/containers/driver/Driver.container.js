6824f223c7741835c499075ac81227f781c268a7
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-20 14:42:40 -0700

    Update Driver.container.js

--- src/containers/driver/Driver.container.js
+++ src/containers/driver/Driver.container.js
@@ -3,16 +3,14 @@ import Card from "../../components/shared/Card";
 
 class Driver extends Component {
   render() {
-    return <div>Driver</div>;
-                <div>Driver #1000</div>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="p-3 bg-white">
-                <div>Driver #1000</div>
-              </div>
-            </div>
     return (
+      <main>
+        <section>
+          <div className="mb-4">
+            <h3>Driver</h3>
+          </div>
+          <div className="row">
+            <Card />
           </div>
         </section>
       </main>
