d045d1ba68616c365cb1f6a8fb48684e4bd20f97
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-20 17:13:28 -0700

    update trip

--- src/containers/trip/TripOngoing.container.js
+++ src/containers/trip/TripOngoing.container.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
-
+import TripOngoingCard from "./tripOngoing.component/TripOngoingCard.component";
+import Pagination from "../../components/shared/Pagination";
 class TripOngoing extends Component {
+  handlePageChange = start => {
+    console.log(start);
+  };
   render() {
-    return <div>TripOngoing</div>;
+    return (
+      <main>
+        <section>
+          <div className="mb-4">
+            <h3>Trip - Ongoing</h3>
+          </div>
+          <div className="row">
+            <TripOngoingCard
+              parentProps={{
+                tripId: "1000016",
+                tripDriver: "Lebron James",
+                tripCustomer: "Kevin Love",
+                tripPickUp: "06/16 16:30 PM",
+                tripFrom: "Pasadena",
+                tripTo: "Arcadia"
+              }}
+            />
+          </div>
+        </section>
+        <Pagination onPageChange={this.handlePageChange} />
+      </main>
+    );
   }
 }
 export default TripOngoing;
---
+++ src/containers/trip/tripOngoing.component/TripOngoingCard.component.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class TripOngoingCard extends Component {
+  render() {
+    const { tripId, tripDriver, tripCustomer, tripPickUp, tripFrom, tripTo } = this.props.parentProps;
+    return (
+      <div className="col-6 col-md-4 col-lg-3 mb-3">
+        <div className="col-12 p-3 shadow-sm bg-white ">
+          <div className="d-flex justify-content-between">
+            <div>{`Trip #${tripId}`}</div>
+            <div className="text-primary">COB</div>
+          </div>
+          <div className="my-2">
+            <div className="font-weight-bold">Driver: {tripDriver}</div>
+            <div className="font-weight-bold">Customer {tripCustomer}</div>
+          </div>
+          <div>
+            <div className="text-muted">Pickup Time: {tripPickUp}</div>
+            <div className="text-muted">From: {tripFrom}</div>
+            <div className="text-muted">To: {tripTo}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
