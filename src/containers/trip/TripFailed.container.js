3412ce8c62d7ae05d9e922dc5db5f9f531e6a605
jeremyguanbusiness <43159269+jeremyguanbusiness@users.noreply.github.com>
2019-06-26 23:09:46 -0700

    update small detail

--- src/containers/trip/TripFailed.container.js
+++ src/containers/trip/TripFailed.container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TripCard from "../../components/shared/TripCard.component";
 
 class TripFailed extends Component {
   render() {
@@ -6,7 +7,19 @@ class TripFailed extends Component {
       <main>
         <section>
           <div className="mb-4">
-            <h3 className="font-weight-bold">Trip - Failed</h3>
+            <h3 className="font-weight-bold">Trip - Abnormal</h3>
+          </div>
+          <div className="row">
+            <TripCard
+              parentProps={{
+                tripId: "1000016",
+                tripDriver: "Lebron James",
+                tripCustomer: "Kevin Love",
+                tripPickUp: "06/16 16:30 PM",
+                tripFrom: "Pasadena",
+                tripTo: "Arcadia"
+              }}
+            />
           </div>
         </section>
       </main>
--- src/containers/trip/TripUpcoming.container.js
+++ src/containers/trip/TripUpcoming.container.js
@@ -8,6 +8,18 @@ class TripUpcoming extends Component {
           <div className="mb-4">
             <h3 className="font-weight-bold">Trip - Upcoming</h3>
           </div>
+          <div className="row">
+            <TripCard
+              parentProps={{
+                tripId: "1000016",
+                tripDriver: "Lebron James",
+                tripCustomer: "Kevin Love",
+                tripPickUp: "06/16 16:30 PM",
+                tripFrom: "Pasadena",
+                tripTo: "Arcadia"
+              }}
+            />
+          </div>
         </section>
       </main>
     );
