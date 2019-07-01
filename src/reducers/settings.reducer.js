1f99fa04863df97e18b573932930afec19cfc590
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-01 14:19:57 -0700

    updatea loard

--- src/reducers/settings.reducer.js
+++ src/reducers/settings.reducer.js
@@ -5,6 +5,16 @@ const initialState = {
     record_list: [],
     count: 0,
     end: 0
+  },
+  vehicle_type_list_in_settings: {
+    record_list: [],
+    count: 0,
+    end: 0
+  },
+  general_list_in_settings: {
+    record_list: [],
+    count: 0,
+    end: 0
   }
 };
 
@@ -12,6 +22,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case constant.COUPON_LIST_IN_SETTINGS:
       return { ...state, coupon_list_in_settings: action.payload };
+    case constant.VEHICLE_TYPE_LIST_IN_SETTINGS:
+      return { ...state, vehicle_type_list_in_settings: action.payload };
+    case constant.GENERAL_LIST_IN_SETTINGS:
+      return { ...state, general_list_in_settings: action.payload };
     default:
       return state;
   }
