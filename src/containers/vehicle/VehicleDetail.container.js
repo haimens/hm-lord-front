389f7e2403357f32c425940992fbf7ab00fcf362
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-09 15:31:32 -0700

    updaett vehicle

--- src/containers/vehicle/VehicleDetail.container.js
+++ src/containers/vehicle/VehicleDetail.container.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { VehicleCard } from "../../components/shared";
-
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { DriverCard, Header, ListHeader } from "../../components/shared";
 import VehicleDetailCard from "./vehicleDetail.component/VehicleDetail.card";
 import AddingDriverModal from "./vehicleDetail.component/AddingDriver.modal";
-export default class VehicleDetail extends Component {
+
+class VehicleDetail extends Component {
   state = {
     showAddingDriverModal: false
   };
@@ -12,34 +14,46 @@ export default class VehicleDetail extends Component {
   };
   render() {
     const { showAddingDriverModal } = this.state;
+    const { history } = this.props;
     return (
       <main>
         {showAddingDriverModal && <AddingDriverModal onClose={this.handleShowAddingDriverModal} />}
         <section className="mb-4">
           <div className="mb-4">
-            <h3 className="font-weight-bold mr-3">Vehicle Detail</h3>
+            <Header
+              title="Driver"
+              subTitle="Vehicle Detail"
+              toLocation={"/vehicle"}
+              tabicon={"tabicon_dashboard.svg"}
+              history={history}
+              buttonWidth={"88px"}
+            />
           </div>
           <div>
             <VehicleDetailCard />
           </div>
         </section>
-        <section className="mb-4">
-          <div className="mb-4 d-flex">
-            <h3 className="font-weight-bold mr-3">Driver List</h3>
-            <i
-              className="fas fa-plus hm-bg-green rounded-circle text-white p-2 hm-pointer-cursor"
-              onClick={this.handleShowAddingVehicleModal}
-            />
-          </div>
-          <div className="row">
-            <VehicleCard
+        <section className="mb-4 bg-white rounded-custom shadow-sm">
+          <ListHeader
+            parentProps={{
+              title: "Related Driver List",
+              clickFunction: this.handleShowAddingVehicleModal,
+              clickTitle: "Vehicle"
+            }}
+            hideShadow={true}
+            buttonWidth={"88px"}
+          />
+          <div className="row p-3">
+            <DriverCard
               parentProps={{
-                vehicleId: "1000016",
-                vehicleName: "Lebron James",
-                vehicleImage: "unnamed.jpg",
-                vehiclePhone: "6266266266"
+                driverName: 123,
+                driverImage: 123,
+                driverPhone: 123,
+                driverUsername: 123,
+                driver_token: 123,
+                isActive: 1
               }}
-              deleteButton={true}
+              history={history}
             />
           </div>
         </section>
@@ -47,3 +61,12 @@ export default class VehicleDetail extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return {};
+};
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(VehicleDetail));
