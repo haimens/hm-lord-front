fafe0eee682cf113cb78ff84da153336524b26bf
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-18 11:28:01 -0700

    for detil

--- src/containers/notification/notificationMessage.component/Chat.modal.js
+++ src/containers/notification/notificationMessage.component/Chat.modal.js
@@ -45,8 +45,10 @@ class ChatModalContainer extends Component {
                   className={`rounded-circle ${message.type === 1 ? "ml-4" : "mr-4"} `}
                 />
                 <div
-                  className={`chat-modal-box hm-text-16 d-flex justify-content-center align-items-center font-weight-500 p-3  ${message.type ===
-                    1 && "messenger-green text-white"} ${message.type === 2 && "messenger-purple text-white"}`}
+                  className={` hm-text-16 d-flex justify-content-center align-items-center font-weight-500 p-3  ${message.type ===
+                    1 && "messenger-green text-white"} ${
+                    message.type === 2 ? "messenger-purple text-white chat-modal-box" : "chat-modal-box-reverse"
+                  }`}
                   style={{ letterSpacing: 0, color: "#8e8e92", lineHeight: "20px" }}
                 >
                   {message.message}
