1088728e875dc99812a0a51ce406cdc7c1aa2e48
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-19 18:48:44 -0700

    Update GeneralEditing.Modal.js

--- src/containers/settings/General.container/General.component/GeneralEditing.Modal.js
+++ src/containers/settings/General.container/General.component/GeneralEditing.Modal.js
@@ -40,52 +40,44 @@ export default class VehicleAdding extends Component {
   render() {
     const { value } = this.state;
     return (
-      <div>
-        <Modal
-          title="Add Key Value"
-          onClose={this.handleClose}
-          position="center"
-          getWidth={"467px"}
-          getHeight={"335px"}
-        >
-          <div className="container">
-            <div className="p-3">
-              <div className="form-group mb-4">
-                <label htmlFor="Driver" className="font-weight-500 hm-text-14 text-secondary-color">
-                  Key
-                </label>
-                <div className="text-modal-color font-weight-bold hm-text-14">{this.props.currSetting.key}</div>
-              </div>
-              <div className="form-group mb-4">
-                <label htmlFor="Driver" className="font-weight-500 hm-text-14 text-secondary-color">
-                  Value
-                </label>
-                <input
-                  type="text"
-                  className="form-control hm-input-height "
-                  name="value"
-                  id="value"
-                  placeholder={"Value"}
-                  value={value}
-                  onChange={this.handleInputChange}
-                />
-              </div>
+      <Modal title="Add Key Value" onClose={this.handleClose} position="center" getWidth={"467px"} getHeight={"335px"}>
+        <div className="container">
+          <div className="p-3">
+            <div className="form-group mb-4">
+              <label htmlFor="Driver" className="font-weight-500 hm-text-14 text-secondary-color">
+                Key
+              </label>
+              <div className="text-modal-color font-weight-bold hm-text-14">{this.props.currSetting.key}</div>
+            </div>
+            <div className="form-group mb-4">
+              <label htmlFor="Driver" className="font-weight-500 hm-text-14 text-secondary-color">
+                Value
+              </label>
+              <input
+                type="text"
+                className="form-control hm-input-height "
+                name="value"
+                id="value"
+                placeholder={"Value"}
+                value={value}
+                onChange={this.handleInputChange}
+              />
+            </div>
 
-              <div className="form-group text-right pt-3">
-                <button
-                  className="button-main-background btn button-main-size px-4 text-white mr-3"
-                  onClick={this.handleCreateADriverInLord}
-                >
-                  Update
-                </button>
-                <button onClick={this.handleClose} className="btn button-main-size btn-outline-secondary px-4">
-                  Cancel
-                </button>
-              </div>
+            <div className="form-group text-right pt-3">
+              <button
+                className="button-main-background btn button-main-size px-4 text-white mr-3"
+                onClick={this.handleCreateADriverInLord}
+              >
+                Update
+              </button>
+              <button onClick={this.handleClose} className="btn button-main-size btn-outline-secondary px-4">
+                Cancel
+              </button>
             </div>
           </div>
-        </Modal>
-      </div>
+        </div>
+      </Modal>
     );
   }
 }
